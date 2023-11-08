91c30cc45c79dbc497af541b849a903703eff05b
korchinmv <korchinmv@yandex.ru>
2023-11-08 11:49:13 +0300

    commit 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,17 @@ import { Titillium_Web } from "next/font/google";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 import Footer from "@/components/Footer";
+import dynamic from "next/dynamic";
 
 const titillium = Titillium_Web({
   weight: ["300", "400", "700"],
   subsets: ["latin"],
+  variable: "--var-titillium",
+});
+
+//Выключаем SSR по необходимости
+const DynamicFooter = dynamic(() => import("./../components/Footer"), {
+  ssr: false,
 });
 
 export default function RootLayout({
@@ -16,11 +23,11 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${titillium.className} flex flex-col h-screen bg-slate-200`}
+        className={`${titillium.variable} flex flex-col h-screen bg-slate-200`}
       >
         <Header />
         <main className='grow container mx-auto px-2'>{children}</main>
-        <Footer />
+        <DynamicFooter />
       </body>
     </html>
   );
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,8 +7,13 @@ export const metadata: Metadata = {
   description: "Users page",
 };
 
+//revalidate-обновляет кэш по запросу каждый час (3600)
 const getUsers = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    next: {
+      revalidate: 3600,
+    },
+  });
 
   if (!res.ok) throw new Error("Ошибка загрузки данных..");
   return res.json();
